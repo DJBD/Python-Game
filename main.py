5de133c5330980f60afaa7c2989b019f8f75dccd
Daniel Duncan <57094924+DJBD@users.noreply.github.com>
2020-11-04 13:23:19 +0000

    settings

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import sys
 import pygame
+from settings import Settings
 
 pygame.init()
 
 
 def start_game():
     # Initialize game and create a screen object.
-    screen = pygame.display.set_mode((1200, 800))
+    set_settings = Settings()
+    screen = pygame.display.set_mode((set_settings.screen_width, set_settings.screen_height))
     pygame.display.set_caption("Cookie Cutter")
+
+
 
     # Start the main loop for the game.
 
     while True:
+        screen.fill(set_settings.background_colour)
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+
+    def __init__(self):
+
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.background_colour = (255, 182, 193)
