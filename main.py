ed0ce4720b9a68d582a3f56f3676a3231456a4e8
Daniel Duncan <57094924+DJBD@users.noreply.github.com>
2020-11-04 13:41:31 +0000

    adding the chef to the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from chef import Chef
 
 pygame.init()
 
@@ -11,12 +12,15 @@ def start_game():
     screen = pygame.display.set_mode((set_settings.screen_width, set_settings.screen_height))
     pygame.display.set_caption("Cookie Cutter")
 
+    chef = Chef(screen)
+
 
 
     # Start the main loop for the game.
 
     while True:
         screen.fill(set_settings.background_colour)
+        chef.blitme()
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
